e545bb76df828d106555c9fa6ec5c01a9cc273de
SashaCattaneo1412 <101577464+SashaCattaneo1412@users.noreply.github.com>
2022-05-19 09:01:13 -0300

    finished ActivateDeactivate

---
+++ Assets/ActivateDeactivate.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActivateDeactivate : MonoBehaviour
+{
+    public GameObject inpuField;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ActivateAndDeactivate()
+    {
+        //if (inpuField.activeInHierarchy)
+        //{
+        //    inpuField.SetActive(false);
+
+        //}
+        //else
+        //{
+        //    inpuField.SetActive(true);
+        //
+        inpuField.SetActive(!inpuField.activeInHierarchy);
+       
+    }
+}
