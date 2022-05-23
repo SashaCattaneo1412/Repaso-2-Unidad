51a746fe35463be3181d18e8a4f447d77999c6c7
SashaCattaneo1412 <101577464+SashaCattaneo1412@users.noreply.github.com>
2022-05-23 10:43:35 -0300

    finished plataforma

---
+++ Assets/PlataformScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlataformScript : MonoBehaviour
+{
+    public float xMovementSpeed;
+    public float xRightLimit;
+    public bool toRight;
+    public float xleftlimit;
+    public GameObject Cilindro;
+    public GameObject Esfera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        toRight = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if (toRight == true )
+        {
+            transform.position += new Vector3(xMovementSpeed, 0, 0);
+        }
+        else
+        {
+            transform.position -= new Vector3(xMovementSpeed, 0, 0);
+        }
+        if (transform.position.x > xRightLimit)
+        {
+            toRight = false;
+            Instantiate(Cilindro);
+
+        }
+        if(transform.position.x < xleftlimit)
+        {
+            toRight = true;
+            Instantiate(Esfera);
+        }
+
+        
+
+    }
+}
